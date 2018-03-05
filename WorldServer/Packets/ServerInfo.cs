62775cfa082f06bb0f92f0bc882db6f13f4bdc8e
cm <cm@bakako.com>
2018-03-06 04:05:00 +0900

    Change Folder and add sql file

---
+++ WorldServer/Packets/ServerInfo.cs
@@ -0,0 +1,24 @@
+﻿using CommonLib;
+using CommonLib.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldServer.Packets
+{
+    public class ServerInfo : ISendPacket
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public uint Port { get; set; }
+        public bool Status { get; set; }
+
+        public BasePacket ToPacket()
+        {
+            return new BasePacket(0x0033, Name.ToTBytes().Concat($"{Address}:{Port.ToString()}".ToTBytes()).ToArray());
+        }
+    }
+}
---
+++ WorldServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using CommonLib;
+using CommonLib.Packets;
+using CommonLib.Socket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldServer
+{
+
+    class Program
+    {
+
+        static InterfaceProtocol[] protocols;
+        static WorldServerProtocol worldServerInstance;
+
+        static void EcoServer_NewRequestReceived(EcoSession session,BasePacket packet)
+        {
+            Logger.Debug($"New Message from {session.SessionID} received, MessageID: {packet.ProtocolID.ToString("X").PadLeft(4, '0')}, Data: {packet.Data.ToHexString()}");
+
+            /*var rtnPacket = */Utilities.CallInterfaceByName(worldServerInstance, ref protocols, session, packet);
+            //if(rtnPacket != null)
+            //{
+            //    session.Send(rtnPacket.ToBytes());
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Logger.Initialize();
+
+            protocols = Utilities.ReadInterfaceConfig("WorldServerInterface.json");
+            worldServerInstance = new WorldServerProtocol();
+            var serverApp = new EcoServerApp(17831, EcoServer_NewRequestReceived);
+            serverApp.Start();
+            Console.ReadLine();
+        }
+    }
+}
